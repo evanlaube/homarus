bacdf45cf3b0af60c06f83a2222f548018f833f1
eLaubster <laubeevan@gmail.com>
2024-01-30 14:24:11 -0600

    Implement new copy constructors

--- test/homarusTest.cpp
+++ test/homarusTest.cpp
@@ -25,10 +25,17 @@ int main() {
     verts.push_back(Vec2d(50, 50));
     verts.push_back(Vec2d(150, 50));
     verts.push_back(Vec2d(100, 150));
-    
+
+    // Steps for creating a new body: 
+    //      1. Create a New shape
+    //      2. Create a fixture and attach shape
+    //      3. Create a body and attach fixture
+    //      4. Add body to world
+
     Polygon *p = new Polygon(verts);
-    Fixture f;
-    f.setShape(p);
+    Fixture f(p);
+
+    delete p;
 
     Body body(Vec2d(200, 100), 0, f);
     world.addBody(body);

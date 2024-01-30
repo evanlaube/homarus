267ae00912fd47e81ec69a0d27b372cc38dd6df0
eLaubster <laubeevan@gmail.com>
2024-01-30 14:21:57 -0600

    Remove includes to prevent circular inclusion

--- src/homarus.h
+++ src/homarus.h
@@ -1,12 +1,9 @@
 
+#ifndef HOMARUS_H
+#define HOMARUS_H
+
 #define TYPE_SHAPE 0
 #define TYPE_CIRCLE 1
 #define TYPE_POLYGON 2
 
-#include "util/vec2.h"
-
-#include "shapes/shape.h"
-
-#include "physics/fixture.h"
-#include "physics/body.h"
-#include "physics/world.h"
+#endif
